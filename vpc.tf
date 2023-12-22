3bdda45c49c63b3dbcbd0ded5d8c6e4e4d03315e
jagdishmodi <48287274+jagdishmodi@users.noreply.github.com>
2023-12-22 12:19:15 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,14 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
+
 # Create VPC Terraform Module
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
